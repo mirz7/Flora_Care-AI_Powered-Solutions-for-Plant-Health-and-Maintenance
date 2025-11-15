8c4d66f900c9914f6aab201b7dbd78aaf958b919
Muhammed Mirza PN <119695383+mirz7@users.noreply.github.com>
2025-11-15 22:40:26 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('all/', views.ProfileListView.as_view(), name='profile-list-view'),
     path('follow/', views.follow_unfollow_profile, name='follow-unfollow-view'),
     path('<int:pk>/', views.ProfileDetailView.as_view(), name='profile-detail-view'),
+    
     path('public-profile/<str:username>/', views.public_profile, name='public-profile'),
     
 ]
